ae0a4d5d3d3f66d0800618cf47d60070d3e05025
V.V.T. <vasilich.tregub@gmail.com>
2024-09-29 21:00:04 +0700

    color image processing added

--- LeGall_wavelet/LeGall_wavelet.py
+++ LeGall_wavelet/LeGall_wavelet.py
@@ -12,9 +12,11 @@ sourceimage = imageio.v3.imread("ducksgrayscale.png")
 # high decomposition level of 9
 imagedecomp = WaveletImage(np.asarray(sourceimage), levels=9)
 
+plt.figure("9-level decomposition")
 plt.imshow(imagedecomp.pixels, cmap="gray")
 plt.show()
 
+plt.figure("Recovered grayscale image from 9-level decomp")
 recovered = imagedecomp.inverse_transform()
 plt.imshow(recovered, cmap="gray")
 plt.show()
---
+++ LeGall_wavelet/LeGall_wavelet_in_color.py
@@ -0,0 +1,55 @@
+
+import PIL.Image
+import numpy as np
+from numpy import ndarray
+from typing import List, Tuple
+from scipy.ndimage import convolve
+import imageio
+from TimCogan_LeGall_wavelet import WaveletImage
+import matplotlib.pyplot as plt
+import PIL
+
+sourceimage = imageio.v3.imread("ducks.png")
+plt.figure("Source image")
+plt.imshow(sourceimage)
+plt.show()
+
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
+ax1.set_xticks([])
+ax1.set_yticks([])
+ax2.set_xticks([])
+ax2.set_yticks([])
+ax3.set_xticks([])
+ax3.set_yticks([])
+
+fig.suptitle("Wavelet decomposition")
+
+lvls=3
+
+red = sourceimage.flatten()[0::3].reshape(sourceimage.shape[0:2])
+reddecomp = WaveletImage(np.asarray(red), levels=lvls)
+
+ax1.imshow(reddecomp.pixels, cmap="Reds")
+
+green = sourceimage.flatten()[1::3].reshape(sourceimage.shape[0:2])
+greendecomp = WaveletImage(np.asarray(green), levels=lvls)
+
+ax2.imshow(greendecomp.pixels, cmap="Greens")
+
+blue = sourceimage.flatten()[2::3].reshape(sourceimage.shape[0:2])
+bluedecomp = WaveletImage(np.asarray(blue), levels=lvls)
+
+ax3.imshow(bluedecomp.pixels, cmap="Blues")
+
+plt.tight_layout()
+plt.show()
+
+plt.figure("Recovered image")
+redrecovered = reddecomp.inverse_transform().astype(np.uint8)
+greenrecovered = greendecomp.inverse_transform().astype(np.uint8)
+bluerecovered = bluedecomp.inverse_transform().astype(np.uint8)
+colorimagedata = np.dstack([redrecovered, greenrecovered, bluerecovered])
+recoveredimage = PIL.Image.fromarray(colorimagedata, "RGB")
+plt.imshow(recoveredimage)
+plt.show()
+
